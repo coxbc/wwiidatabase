5a8f2123dc46af96c7029acc6939f75bc3085d8f
pumpdude93@gmail.com <pumpdude93@gmail.com@52e99533-fc22-3a68-aa71-eac577140bda>
2013-01-31 04:55:23 +0000

    Added A login and register. the WWII file will be overwritten eventually. Also this code is using mysql and it should be changed to sql server

---
+++ Login.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+
+<?php
+// Open a connection to the database
+// (display an error if the connection fails)
+$conn = mysqli_connect('localhost', 'Admin', 'F8eGy7WtY3HScSnU') or die(mysqli_error());
+mysqli_select_db($conn, 'logintest') or die(mysqli_error());
+
+//This is supposed to start a session
+//session_start();
+//session_destroy();
+//$test_session = "This is a variable session";
+//session_start("test_session");
+
+
+?>
+
+<html>
+
+
+<html>
+<head>
+<title>Login</title>
+</head>
+<body>
+
+<h1>Login Test!</h1>
+<?php
+// Only execute if we're receiving a post
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    // This will be the string we collect errors in
+    $errors = '';
+
+
+    // Make sure the username field is filled
+    $username = $_POST['username'];
+    $username = mysqli_real_escape_string($conn, $username);
+    $username = htmlspecialchars($username, ENT_COMPAT | ENT_HTML401);
+    if (empty($username)) $errors .= '<li>Username is required</li>';
+
+    // Make sure the password field is filled
+    $password = $_POST['password'];
+    $password = mysqli_real_escape_string($conn, $password);
+    $password = htmlspecialchars($password, ENT_COMPAT | ENT_HTML401);
+    if (empty($password)) $errors .= '<li>Password is required</li>';
+
+    // If we have any errors at this point, stop here and show them
+    if (!empty($errors)) {
+        echo '<ul>' . $errors . '</ul>';
+
+    // Otherwise, begin the user creation process
+    } else {
+     // First, check for that username already being taken
+    $user_results = mysqli_query($conn, "SELECT * FROM users WHERE Username = '" . $username . "'");
+    // We don't care what the result is
+    // If there is one, that means the username is taken
+    if ($user_results) {
+		//stuff for session maybe
+		//echo "<?" . 
+		//"session_start();" . 
+		//"$test_session = 'This is a variable session';" .
+		//"session_start('test_session');" . 
+		header( "location: /wwiidatabase/WWII.php" );
+	
+    }
+
+    }
+}
+?>
+
+<form action="" method="post">
+	<label for="username">Username</label><br/>
+	<input type="text" name="username"/><br/>
+	<label for="username">Password</label><br/>
+	<input type="password" name="password"/><br/>
+    <input type="submit" value="Login"/><br/>
+</form>
+<a href="/wwiidatabase/Register.php" style="float:right">New User?</a>
+
+
+
+</body>
+</html>
---
+++ WWII.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>WWII</title>
+<script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyDY0kkJiTPVd2U7aTOAwhc9ySH6oHxOIYM&sensor=false">
+</script>
+<link rel="stylesheet" type="text/css" href="styles.css" media="screen" />
+<script>
+function initialize()
+{
+var mapProp = {
+  center:new google.maps.LatLng(51.508742,-0.120850),
+  zoom:5,
+  mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+var map=new google.maps.Map(document.getElementById("googleMap")
+  ,mapProp);
+};
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+</script>
+    <script type="text/javascript">
+      function initialize() {
+        var mapOptions = {
+          zoom: 4,
+          center: new google.maps.LatLng(-25.363882, 131.044922),
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+
+        var map = new google.maps.Map(document.getElementById("map_canvas"),
+            mapOptions);
+
+        var marker = new google.maps.Marker({
+          position: map.getCenter(),
+          map: map,
+          title: 'Click to zoom'
+        });
+
+        google.maps.event.addListener(map, 'center_changed', function() {
+          // 3 seconds after the center of the map has changed, pan back to the
+          // marker.
+          window.setTimeout(function() {
+            map.panTo(marker.getPosition());
+          }, 3000);
+        });
+
+        google.maps.event.addListener(marker, 'click', function() {
+          map.setZoom(8);
+          map.setCenter(marker.getPosition());
+        });
+      }
+
+      google.maps.event.addDomListener(window, 'load', initialize);
+	</script>
+	<script>
+	function testphpCall(){
+	alert("check");
+	}
+	</script>
+</head>
+<body>
+
+<h1>WWII</h1>
+<?php
+	function test(){
+	echo 'Test';
+	echo '<script type="text/javascript">'
+   , 'testphpCall();'
+   , '</script>';
+	}
+   ?>
+<div id="map_canvas" style="width:100%; height:100%"></div>
+<div id="googleMap" style="width: 100%; height: 700px; border:1px solid gray;""></div>
+<div id="slider" style="width: 100%; position:absolute; margin: 0 auto;" >
+<div style="float: left">Start of WWII</div> <div style="float: right">END of WWII</div>
+<input id="slide" type="range"
+ min="5" max="200" step="5" value="100"
+ onchange="updateSlider(this.value)" style="width: 100%;"/>
+</div><br/><br/>
+
+	<tr>
+
+		
+		<td id="logoTbl">
+			<label><strong>Type</strong></label>
+			<select onchange="test()">
+			<option value="volvo"">Volvo</option>
+			<option value="saab">Saab</option>
+			<option value="mercedes">Mercedes</option>
+			<option value="audi">Audi</option>
+			</select>
+		</td>
+	</tr>
+</table>
+</body>
+</html>
