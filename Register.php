b4a910731d98d72781f64639af7f2de543f6c355
pumpdude93@gmail.com <pumpdude93@gmail.com@52e99533-fc22-3a68-aa71-eac577140bda>
2013-02-08 07:07:27 +0000

    Back up for myself

--- Register.php
+++ Register.php
@@ -1,10 +1,14 @@
 <!DOCTYPE html>
 
 <?php
-// Open a connection to the database
-// (display an error if the connection fails)
-$conn = mysqli_connect('localhost', 'Admin', 'F8eGy7WtY3HScSnU') or die(mysqli_error());
-mysqli_select_db($conn, 'logintest') or die(mysqli_error());
+$server = 'whale.csse.rose-hulman.edu';
+
+// Connect to MSSQL
+$link = odbc_connect("Driver={SQL Server Native Client 10.0};Server=whale.csse.rose-hulman.edu;Database=Nadine_WWII", 'coxbc', 'whales');
+
+if (!$link) {
+    die('Something went wrong while connecting to MSSQL');
+}
 ?>
 
 <html>
@@ -23,23 +27,31 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $errors = '';
 
     // Make sure the name field is filled
-    $name =  $_POST['name'];
-    $name = mysqli_real_escape_string($conn, $name);
-    $name = htmlspecialchars($name, ENT_COMPAT | ENT_HTML401);  
-    if (empty($name)) $errors .= '<li>Name is required</li>';
+    $fname =  $_POST['fname'];
+    $fname =  odbc_access_escape_str($fname);
+    $fname = htmlspecialchars($fname, ENT_COMPAT | ENT_HTML401);  
+    if (empty($fname)) $errors .= '<li>First Name is required</li>';
+	
+	$lname =  $_POST['lname'];
+    $lname = odbc_access_escape_str($lname);
+    $lname = htmlspecialchars($lname, ENT_COMPAT | ENT_HTML401);  
+    if (empty($lname)) $errors .= '<li>Last Name is required</li>';
+	$email =  $_POST['email'];
+    $email = odbc_access_escape_str($email);
+    $email = htmlspecialchars($email, ENT_COMPAT | ENT_HTML401);  
+    if (empty($email)) $errors .= '<li>Email is required</li>';
 
     // Make sure the username field is filled
     $username = $_POST['username'];
-    $username = mysqli_real_escape_string($conn, $username);
+    $username = odbc_access_escape_str($username);
     $username = htmlspecialchars($username, ENT_COMPAT | ENT_HTML401);
-    if (empty($name)) $errors .= '<li>Username is required</li>';
+    if (empty($username)) $errors .= '<li>Username is required</li>';
 
     // Make sure the password field is filled
     $password = $_POST['password'];
-    $password = mysqli_real_escape_string($conn, $password);
+    $password = odbc_access_escape_str($password);
     $password = htmlspecialchars($password, ENT_COMPAT | ENT_HTML401);
     if (empty($password)) $errors .= '<li>Password is required</li>';
-
     // Make sure the passwords match
     $confirm = $_POST['confirmpassword'];
     if (strcmp($password, $confirm) != 0) $errors .= '<li>Passwords do not match</li>';
@@ -51,42 +63,63 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // Otherwise, begin the user creation process
     } else {
      // First, check for that username already being taken
-    $user_results = mysqli_query($conn, "SELECT Username FROM users 
-WHERE Username = " . $username);
+    $user_results = odbc_exec($link, "SELECT Username FROM Users WHERE Username = '" . $username . "'");
 
     // We don't care what the result is
     // If there is one, that means the username is taken
-    if ($user_results) {
-    	if (mysqli_fetch_array($user_results)) {
+    if (odbc_fetch_row($user_results)) {
         	echo '<ul><li>Username already taken</li></ul>';
-	}
     // If no duplicates are found, go ahead and create the new user
     } else {
-
-mysqli_query($conn,"INSERT INTO users (Name, Username, Password) " .
-                    "VALUES ('" . $name . "', '" . 
+	echo '<script>alert("Check")</script>';
+	odbc_exec($link,"INSERT INTO Users (FName, LName, Email, Username, Password) " .
+                    "VALUES ('" . $fname . "', '" . $lname . "','" . $email . "', '" . 
                                   $username .
-                                  "', saltedHash('" . $username . "', '" .
-                                  $password . "'))") or die(‘error’);
+                                  "', '" . sha1("'" . $username . $password . "'") . "')") or die(‘error’);
+								  
 
         // Show a success message
         echo '<ul><li>Registration successful!</li></ul>';
+		$_SESSION['loggedin']=TRUE;
 
         // Set the name and username fields to empty strings so they don't
         // get automatically repopulated
-        $name = '';
+        $fname = '';
+		$lname = '';
+		$email = '';
         $username = '';
+		header( "location: /WWII.php" );
     }
 
     }
 }
 ?>
+<?php
+function odbc_access_escape_str($str) {
+ $out="";
+ for($a=0; $a<strlen($str); $a++) {
+  if($str[$a]=="'") {
+   $out.="''";
+  } else
+  if($str[$a]!=chr(10)) {
+   $out.=$str[$a];
+  } 
+ }
+ return $out; 
+}
+?>
 
 
 <form action="" method="post">
-    <label for="name">Name</label><br/>
-    <input type="text" name="name"/><br/>
+    <label for="fname">First Name</label><br/>
+    <input type="text" name="fname"/><br/>
+	
+	<label for="lname">Last Name</label><br/>
+    <input type="text" name="lname"/><br/>
 
+	<label for="email">Email</label><br/>
+    <input type="text" name="email"/><br/>
+	
     <label for="username">Username</label><br/>
     <input type="text" name="username"/><br/>
 
@@ -95,7 +128,7 @@ mysqli_query($conn,"INSERT INTO users (Name, Username, Password) " .
 
     <label for="confirmpassword">Confirm Password</label><br/>
     <input type="password" name="confirmpassword"/><br/>
-
+	
     <input type="submit" value="Register"/>
 </form>
 
--- WWII.php
+++ WWII.php
@@ -1,104 +1,248 @@
 <!DOCTYPE html>
 <?php
+$server = 'whale.csse.rose-hulman.edu';
 
-session_start();
-if ($_SESSION['loggedin'] !== TRUE) {
-   header("Location: /wwiidatabase/Login.php");
-   echo 'You must log in first';
-   exit();
+// Connect to MSSQL
+$link = odbc_connect("Driver={SQL Server Native Client 10.0};Server=whale.csse.rose-hulman.edu;Database=Nadine_WWII", 'coxbc', 'whales');
+
+if (!$link) {
+    die('Something went wrong while connecting to MSSQL');
 }
+?>
+<?php
+session_cache_expire( 20 );
+session_start(); // NEVER FORGET TO START THE SESSION!!!
+$inactive = 300;
+if(isset($_SESSION['start']) ) {
+	$session_life = time() - $_SESSION['start'];
+	if($session_life > $inactive){
+		$_SESSION['loggedin'] = false;
+		session_destroy();
+		header("Location: /Login.php");
+	}
+}
+$_SESSION['start'] = time();
+if($_SESSION['loggedin'] != true){
+header("Location: /Login.php");
+exit();
+}
+
+
+
+
+//session_start();
+//if ($_SESSION['loggedin'] !== TRUE) {
+  // header("Location: /Login.php");
+   //echo 'You must log in first';
+   //exit();
+   
+//$inactive = 5; 
+//$session_life = time() - $_session['timeout'];
+//if($session_life > $inactive)
+//{  session_destroy(); header("Location: /Login.php");     }
+//$_session['timeout']=time();
+//}
 ?>
 <html>
 <head>
-
 <title>WWII</title>
-<script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyDY0kkJiTPVd2U7aTOAwhc9ySH6oHxOIYM&sensor=false">
+<script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyBf_PyFp4dnlbBqm_H3cALDykYQm8dUKBI&sensor=false">
 </script>
 <link rel="stylesheet" type="text/css" href="styles.css" media="screen" />
 <script>
+
+var markersArray = [];
+
 function initialize()
 {
-var mapProp = {
-  center:new google.maps.LatLng(51.508742,-0.120850),
-  zoom:5,
-  mapTypeId:google.maps.MapTypeId.ROADMAP
-  };
-var map=new google.maps.Map(document.getElementById("googleMap")
-  ,mapProp);
-};
+	var mapProp = {
+	  center:new google.maps.LatLng(51.508742,-0.120850),
+	  zoom:5,
+	  mapTypeId:google.maps.MapTypeId.ROADMAP
+	};
+	this.map=new google.maps.Map(document.getElementById("googleMap"),mapProp);	
+	
+	var styles = [
+  {
+    featureType: "road",
+    stylers: [
+      { visibility: "off" }
+    ]
+  },{
+	featureType: "poi",
+	stylers: [
+		{ visibility: "simplified" }
+	]
+	},{
+	featureType: "administrative.province",
+	elementType: "geometry.stroke",
+	stylers: [
+		{visibility: "off"}
+	]
+	
+	}
+  
+];
+
+	map.setOptions({styles: styles});
+	
+	if (window.XMLHttpRequest)
+   {// code for IE7+, Firefox, Chrome, Opera, Safari
+		xmlhttp=new XMLHttpRequest();
+   }
+	
+	xmlhttp.onreadystatechange = function()
+	{
+		var element = "";
+		if (xmlhttp.readyState==4 && xmlhttp.status==200){
+//			document.getElementById("txtHint").innerHTML=xmlhttp.responseText;
+			element = xmlhttp.responseText
+		}
+		
+		
+		var battles = element.split("<br>");
+		
+		var i = 0;
+		while(i < battles.length){
+		
+			var battletbl = battles[i].split(",");
+			var name = battletbl[0];
+			var id = parseInt(battletbl[1]);
+			
+			var lat = parseFloat(battletbl[2]);
+			var longi = parseFloat(battletbl[3]);
+			
+
+			var marker = new google.maps.Marker({
+			position: new google.maps.LatLng(lat, longi),
+			map: map,
+			title: name
+			});
+			
+			markersArray.push(marker);
+			
+			i = i + 1;
+		}
+		
+		
+	}
+	xmlhttp.open("GET","dbaccess.php?" +"country=" + $_GET['countrytbl'],true);
+	xmlhttp.send();
+
+}
+
+function clearOverlays() {
+  for (var i = 0; i < markersArray.length; i++ ) {
+    markersArray[i].setMap(null);
+  }
+}
+
+function TestFunction(){
+	
+	if (window.XMLHttpRequest)
+   {// code for IE7+, Firefox, Chrome, Opera, Safari
+		xmlhttp=new XMLHttpRequest();
+   }
+	
+	xmlhttp.onreadystatechange = function()
+	{
+	
+		clearOverlays();
+	/*
+		for ( i in markersarray){
+	//		alert("Got one");
+			markersarray[i].setMap(null);
+			map.removeOverlay(markersarray[i]);
+	//		markersarray.length = 0;
+		}
+	*/
+		var element = "";
+		if (xmlhttp.readyState==4 && xmlhttp.status==200){
+//			document.getElementById("txtHint").innerHTML=xmlhttp.responseText;
+			element = xmlhttp.responseText
+		}
+		
+				var battles = element.split("<br>");
+		
+		var i = 0;
+		while(i < battles.length){
+		
+			var battletbl = battles[i].split(",");
+			var name = battletbl[0];
+			var id = parseInt(battletbl[1]);
+			
+			var lat = parseFloat(battletbl[2]);
+			var longi = parseFloat(battletbl[3]);
+			
+			var marker = new google.maps.Marker({
+			position: new google.maps.LatLng(lat, longi),
+			map: map,
+			title: name
+			});
+			
+			markersArray.push(marker);
+			GEvent.addListener(marker, "click", function()
+{marker.openInfoWindowHtml('here I am');});
+
+			i = i + 1;
+		}
+		
+	}
+//	alert("OHNO");
+//	alert(document.getElementById("countrytable").value);
+	if(document.getElementById("countrytable").value == "Any"){
+		xmlhttp.open("GET","dbaccess.php",true);
+	}
+	else{
+		xmlhttp.open("GET","dbaccess.php?" +"country=" + document.getElementById("countrytable").value,true);
+	}
+	xmlhttp.send();
+		
+}	
 
 google.maps.event.addDomListener(window, 'load', initialize);
 
 </script>
-    <script type="text/javascript">
-      function initialize() {
-        var mapOptions = {
-          zoom: 4,
-          center: new google.maps.LatLng(-25.363882, 131.044922),
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-
-        var map = new google.maps.Map(document.getElementById("map_canvas"),
-            mapOptions);
-
-        var marker = new google.maps.Marker({
-          position: map.getCenter(),
-          map: map,
-          title: 'Click to zoom'
-        });
-
-        google.maps.event.addListener(map, 'center_changed', function() {
-          // 3 seconds after the center of the map has changed, pan back to the
-          // marker.
-          window.setTimeout(function() {
-            map.panTo(marker.getPosition());
-          }, 3000);
-        });
-
-        google.maps.event.addListener(marker, 'click', function() {
-          map.setZoom(8);
-          map.setCenter(marker.getPosition());
-        });
-      }
-
-      google.maps.event.addDomListener(window, 'load', initialize);
-	</script>
-	<script>
-	function testphpCall(){
-	alert("check");
-	}
-	</script>
 </head>
 <body>
 
 <h1>WWII</h1>
-<?php
-	function test(){
-	echo 'Test';
-	echo '<script type="text/javascript">'
-   , 'testphpCall();'
-   , '</script>';
-	}
-   ?>
-<div id="map_canvas" style="width:100%; height:100%"></div>
+
 <div id="googleMap" style="width: 100%; height: 700px; border:1px solid gray;""></div>
-<div id="slider" style="width: 100%; position:absolute; margin: 0 auto;" >
+<div id="slider" style="width: 98%; position:absolute; margin: 0 auto;" >
 <div style="float: left">Start of WWII</div> <div style="float: right">END of WWII</div>
 <input id="slide" type="range"
  min="5" max="200" step="5" value="100"
  onchange="updateSlider(this.value)" style="width: 100%;"/>
 </div><br/><br/>
-
+<table cellspacing="0" name="comboBoxes">
 	<tr>
 
-		
 		<td id="logoTbl">
-			<label><strong>Type</strong></label>
-			<select onchange="test()">
-			<option value="volvo"">Volvo</option>
-			<option value="saab">Saab</option>
-			<option value="mercedes">Mercedes</option>
-			<option value="audi">Audi</option>
+			<label><strong>Countries</strong></label>
+			<select id = "countrytable" name = "countrytbl" ONCHANGE = "TestFunction()">
+			<?php
+				echo '<option>' . "Any" . '</option>';
+				$data = odbc_exec($link, "SELECT Name FROM Country ORDER BY Name");
+
+				while (odbc_fetch_row($data)) {
+				echo '<option>' . odbc_result($data, 1) . '</option>';
+				}
+			?>
+			</select>
+		</td>
+
+		<td id="logoTbl">
+		
+			<label><strong>Commanders</strong></label>
+			<select name = "cmdrtbl" ONCHANGE = "TestFunction()">
+			<?php
+
+				$data = odbc_exec($link, "SELECT Name FROM Commander ORDER BY Name");
+				while (odbc_fetch_row($data)) {
+				echo '<option value=odbc_result($data, 1)>' . odbc_result($data, 1) . '</option>';
+				}
+			?>
 			</select>
 		</td>
 	</tr>
